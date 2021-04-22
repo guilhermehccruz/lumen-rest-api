0d299f4c3140d1569cc599be083959e31c16bbcb
Guilherme Cruz <71985319+guilhermehccruz@users.noreply.github.com>
2021-04-22 17:13:43 -0300

    Auth done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,97 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+
+        // Check if any field is empty
+        if (empty($email) or empty($password)) {
+            return response()->json(['status' => 'error', 'message' => 'You must fill all the fields']);
+        }
+
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function register(Request $request)
+    {
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+
+        // Check if any field is empty
+        if (empty($name) or empty($email) or empty($password)) {
+            return response()->json(['status' => 'error', 'message' => 'You must fill all the fields']);
+        }
+
+        // Check if the email is valid
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return response()->json(['status' => 'error', 'message' => 'You must enter a valid email']);
+        }
+
+        // Check if the password is at least 6 characters long
+        if (strlen($password) < 6) {
+            return response()->json(['status' => 'error', 'message' => 'Password must be min 6 characters']);
+        }
+
+        // Check if this email already exists
+        if (User::where('email', $email)->exists()) {
+            return response()->json(['status' => 'error', 'message' => 'This email is unavailable']);
+        }
+
+        // Create new user
+        try {
+            $user = new User();
+            $user->name = $name;
+            $user->email = $email;
+            $user->password = app('hash')->make($password);
+
+            if ($user->save()) {
+                return $this->login($request);
+            }
+        } catch (Exception $ex) {
+            return response()->json(['status' => 'error', 'message' => $ex->getMessage()]);
+        }
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use App\Models\Post;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -17,9 +19,18 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+
+
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/posts', 'PostController@index');
-    $router->post('/posts', 'PostController@store');
-    $router->put('/posts/{id}', 'PostController@update');
-    $router->delete('/posts/{id}', 'PostController@destroy');
+    $router->post('login', 'AuthController@login');
+    $router->post('/register', 'AuthController@register');
+
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->post('/logout', 'AuthController@logout');
+
+        $router->get('/posts', 'PostController@index');
+        $router->post('/posts', 'PostController@store');
+        $router->put('/posts/{id}', 'PostController@update');
+        $router->delete('/posts/{id}', 'PostController@destroy');
+    });
 });
